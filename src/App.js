fa34872ccca76d41f80f210c26986d479721c49d
Lohit754 <34985601+Lohit754@users.noreply.github.com>
2018-10-16 21:05:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,10 +29,10 @@ class App extends Component {
   }
 
   checkForWinner() {
-    var currentTurn = this.state.currentTurn
-    var symbols = this.state.board
-    var winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-    return winningCombos.find(function(combo) {
+    const currentTurn = this.state.currentTurn
+    const symbols = this.state.board
+    const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    return winningCombos.find((combo) => {
       if(symbols[combo[0]] !== "" && symbols[combo[1]] !== ""  && symbols[combo[2]] !== ""  && symbols[combo[0]] === symbols[combo[1]] && symbols[combo[1]] === symbols[combo[2]]) {
         return currentTurn
       } else {
